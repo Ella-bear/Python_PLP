bfc9613f8eb2ef7722cccf8e723b328cc08c6125
Ella-bear <sheila.5wambui@gmail.com>
2025-04-01 17:35:16 +0300

    first commit

--- day4/error_handling.py
+++ day4/error_handling.py
@@ -1,9 +1,10 @@
 try:
     #asks users to input their filename
-    filename = input("Enter the filename to read: ")
+    filename = input("Enter your filename to read: ")
 
     with open(filename, 'r') as file:
         content = file.read()
         print(content)
 except FileNotFoundError:
     print("Error: The file you are trying to read does not exist. Please check the file path and try again.")
+
